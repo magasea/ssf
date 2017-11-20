48597deea9da469ebdd16c86b6ee4f5bb9699099
chenwei <david.chen@shellshellfish.com>
2017-11-20 20:29:36 +0800

    update for properties file

--- aaas-api-user-info/src/main/java/com/shellshellfish/aaas/userinfo/model/dao/userinfo/UiTrdLog.java
+++ aaas-api-user-info/src/main/java/com/shellshellfish/aaas/userinfo/model/dao/userinfo/UiTrdLog.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
 
 
 /**
@@ -40,6 +42,7 @@ public class UiTrdLog implements Serializable {
   @Column(name="prod_id")
   private BigInteger prodId;
 
+  @Indexed(name="trade_date_idx", direction = IndexDirection.DESCENDING)
   @Column(name="trade_date")
   private Long tradeDate;
 
