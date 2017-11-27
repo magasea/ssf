3170bf679629caab340166b137f173b2b6e85994
chenwei <david.chen@shellshellfish.com>
2017-11-27 17:57:01 +0800

    add gateway api

---
+++ aaas-api-gateway/src/main/java/com/shellshellfish/aaas/gateway/filters/PostFilter.java
@@ -0,0 +1,32 @@
+package com.shellshellfish.gateway.filters;
+
+
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+
+@Component
+public class PostFilter extends ZuulFilter {
+
+	@Override
+	public Object run() {
+	    System.out.println("Inside post filter");
+	    return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+	@Override
+	public String filterType() {
+		return "post";
+	}
+
+}
