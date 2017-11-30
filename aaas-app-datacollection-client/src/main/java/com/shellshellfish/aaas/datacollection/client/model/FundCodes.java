858b12f0e0900da1eceb4062c73a0db0153f99ca
chenwei <david.chen@shellshellfish.com>
2017-11-30 14:40:42 +0800

    adjust and add grpc service

---
+++ aaas-app-datacollection-client/src/main/java/com/shellshellfish/aaas/datacollection/client/model/FundCodes.java
@@ -0,0 +1,14 @@
+package com.shellshellfish.aaas.datacollection.client.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class FundCodes {
+  String id;
+  String code;
+  String name;
+  String date;
+}
--- aaas-app-datacollection/src/main/java/com/shellshellfish/aaas/repositories/CarMongoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shellshellfish.aaas.repositories;
-
-
-import com.shellshellfish.aaas.model.Car;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CarMongoRepository extends CrudRepository<Car, String> {}
