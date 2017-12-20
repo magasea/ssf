dd20452fff2591f9929099278c685d74ca2d5d29
shitong <tong.shi@shellshellfish.com>
2017-12-20 12:09:33 +0800

    update

--- aaas-api-user-info/src/main/java/com/shellshellfish/aaas/userinfo/model/dao/UiSysMsg.java
+++ aaas-api-user-info/src/main/java/com/shellshellfish/aaas/userinfo/model/dao/UiSysMsg.java
@@ -9,40 +9,103 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Setter
 @Document(collection = "ui_sysmsg")
 public class UiSysMsg {
-  private static final long serialVersionUID = 1L;
-  @Id
-  private String id;
+	private static final long serialVersionUID = 1L;
 
-  private String source;
+	@Id
+	private String id;
 
-  private String content;
+	private String source;
 
+	private String content;
 
-  private String createdBy;
+	private String createdBy;
 
+	private Long createdDate;
 
-  private Long createdDate;
+	private String lastModifiedBy;
 
-  private String lastModifiedBy;
+	private Long lastModifiedDate;
 
-  private Long lastModifiedDate;
+	private String date;
 
-  private Long date;
+	private String title;
 
-  private String title;
+	public String getId() {
+		return id;
+	}
 
+	public void setId(String id) {
+		this.id = id;
+	}
 
-  @Override
-  public String toString() {
-    return "UiSysMsg{" +
-        ", content='" + content + '\'' +
-        ", source='" + source + '\'' +
-        ", createdBy='" + createdBy + '\'' +
-        ", createdDate=" + createdDate +
-        ", lastModifiedBy='" + lastModifiedBy + '\'' +
-        ", lastModifiedDate=" + lastModifiedDate +
-        ", date=" + date +
-        ", title='" + title + '\'' +
-        '}';
-  }
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Long getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Long createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getLastModifiedBy() {
+		return lastModifiedBy;
+	}
+
+	public void setLastModifiedBy(String lastModifiedBy) {
+		this.lastModifiedBy = lastModifiedBy;
+	}
+
+	public Long getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(Long lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public String toString() {
+		return "UiSysMsg{" + ", content='" + content + '\'' + ", source='" + source + '\'' + ", createdBy='" + createdBy
+				+ '\'' + ", createdDate=" + createdDate + ", lastModifiedBy='" + lastModifiedBy + '\''
+				+ ", lastModifiedDate=" + lastModifiedDate + ", date=" + date + ", title='" + title + '\'' + '}';
+	}
 }
--- aaas-api-user-info/src/main/java/com/shellshellfish/aaas/userinfo/model/dto/UserSysMsgDTO.java
+++ aaas-api-user-info/src/main/java/com/shellshellfish/aaas/userinfo/model/dto/UserSysMsgDTO.java
@@ -6,8 +6,41 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserSysMsgDTO {
-  String source;
-  String content;
-  String id;
-  Long date;
+	String source;
+	String content;
+	String id;
+	String date;
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
 }
