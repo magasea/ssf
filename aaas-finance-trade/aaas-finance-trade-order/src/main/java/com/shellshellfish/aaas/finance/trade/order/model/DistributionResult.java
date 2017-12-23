3aec4497fe128486c95320cf4772f616e7a7775d
liuxiaowei <levy.liu@shellshellfish.com>
2017-12-23 11:52:48 +0800

    poundage calculation and distribution in one api call

--- aaas-finance-trade/aaas-finance-trade-order/src/main/java/com/shellshellfish/aaas/finance/trade/order/model/DistributionResult.java
+++ aaas-finance-trade/aaas-finance-trade-order/src/main/java/com/shellshellfish/aaas/finance/trade/order/model/DistributionResult.java
@@ -1,18 +1,29 @@
 package com.shellshellfish.aaas.finance.trade.order.model;
 
 import java.math.BigDecimal;
+import java.util.List;
 
-public class PoundageResult {
+public class DistributionResult {
     private BigDecimal poundage;
     private BigDecimal discountSaving;
+    private List<FundAmount> fundAmountList;
 
-    public PoundageResult() {
+    public List<FundAmount> getFundAmountList() {
+        return fundAmountList;
+    }
+
+    public void setFundAmountList(List<FundAmount> fundAmountList) {
+        this.fundAmountList = fundAmountList;
+    }
+
+    public DistributionResult() {
 
     }
 
-    public PoundageResult(BigDecimal poundage, BigDecimal discountSaving) {
+    public DistributionResult(BigDecimal poundage, BigDecimal discountSaving, List<FundAmount> fundAmountList) {
         this.poundage = poundage;
         this.discountSaving = discountSaving;
+        this.fundAmountList = fundAmountList;
     }
 
     public BigDecimal getPoundage() {
