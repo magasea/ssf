2582cb19a07786ba21597237d2927aeaddce4bda
xuqie <xuqie@shellshellfish.com>
2017-12-26 14:20:35 +0800

    process added

--- aaas-mid-api/src/main/java/com/shellshellfish/aaas/transfer/controller/TransferController.java
+++ aaas-mid-api/src/main/java/com/shellshellfish/aaas/transfer/controller/TransferController.java
@@ -6,13 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.shellshellfish.aaas.dto.FinanceProdBuyInfo;
 import com.shellshellfish.aaas.model.JsonResult;
+import com.shellshellfish.aaas.service.MidApiService;
 import com.shellshellfish.aaas.transfer.exception.ReturnedException;
 
 import io.swagger.annotations.Api;
@@ -29,6 +33,9 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/phoneapi-ssf")
 @Api("转换相关restapi")
 public class TransferController {
+	
+	@Autowired
+	private MidApiService service;
 	
 	@Autowired
 	private RestTemplate restTemplate;
@@ -61,17 +68,28 @@ public class TransferController {
 	
 	@ApiOperation("申购基金")
 	@ApiImplicitParams({
-		@ApiImplicitParam(paramType="query",name="telNum",dataType="String",required=true,value="",defaultValue="")
+		@ApiImplicitParam(paramType="query",name="telNum",dataType="String",required=true,value="电话号码",defaultValue=""),
+		@ApiImplicitParam(paramType="query",name="msgCode",dataType="String",required=true,value="验证码",defaultValue="")
 	})
 	@RequestMapping(value="/subscribeFund",method=RequestMethod.POST)
 	@ResponseBody
-	public JsonResult doTransaction(){
-		//首先验证验证码
+	public JsonResult doTransaction(@RequestParam String telNum,@RequestParam String msgCode,@RequestBody FinanceProdBuyInfo prdInfo){
+		//首先验证验证码\
+		try{
+		String verify=service.verifyMSGCode(telNum, msgCode);
+		if ("验证失败".equals(verify)){
+			return new JsonResult(JsonResult.Fail,"手机验证失败", "");
+		}
 		
 		
 		
 		
-		return new JsonResult(JsonResult.SUCCESS, "请求成功", null);
+		}catch(Exception e){
+			String str=new ReturnedException(e).getErrorMsg();
+			return new JsonResult(JsonResult.Fail, str, "");
+		}
+
+		return new JsonResult(JsonResult.SUCCESS, "购买成功", null);
 	}
 	
 }
