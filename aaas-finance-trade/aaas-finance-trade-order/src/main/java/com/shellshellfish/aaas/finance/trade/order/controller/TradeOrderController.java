51bb54e7f591c0ec1f648dba932f015b50bd7c12
shitong <tong.shi@shellshellfish.com>
2017-12-27 15:14:17 +0800

    update

--- aaas-finance-trade/aaas-finance-trade-order/src/main/java/com/shellshellfish/aaas/finance/trade/order/controller/TradeOrderController.java
+++ aaas-finance-trade/aaas-finance-trade-order/src/main/java/com/shellshellfish/aaas/finance/trade/order/controller/TradeOrderController.java
@@ -1,13 +1,22 @@
 package com.shellshellfish.aaas.finance.trade.order.controller;
 
 import com.shellshellfish.aaas.finance.trade.order.model.dao.TrdOrder;
+import com.shellshellfish.aaas.finance.trade.order.model.dao.TrdOrderDetail;
+
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +28,7 @@ import com.shellshellfish.aaas.finance.trade.order.model.DistributionResult;
 import com.shellshellfish.aaas.finance.trade.order.model.vo.FinanceProdBuyInfo;
 import com.shellshellfish.aaas.finance.trade.order.service.FinanceProdCalcService;
 import com.shellshellfish.aaas.finance.trade.order.service.FinanceProdInfoService;
+import com.shellshellfish.aaas.finance.trade.order.service.OrderService;
 import com.shellshellfish.aaas.finance.trade.order.service.TradeOpService;
 
 import io.swagger.annotations.ApiImplicitParam;
@@ -42,6 +52,9 @@ public class TradeOrderController {
   
   @Autowired
   FinanceProdCalcService financeProdCalcService;
+  
+  @Autowired
+  OrderService orderService;
 
 
 
@@ -107,6 +120,89 @@ public class TradeOrderController {
 		return new ResponseEntity<DistributionResult>(distributionResult, HttpStatus.OK);
 	} 
 
-
-
+	/**
+	   * 购买理财产品 产品详情
+	   *
+	   * @param totalAmount
+	   * @return
+	   */
+	@ApiOperation("购买理财产品 产品详情页面")
+	@ApiImplicitParams({
+//				@ApiImplicitParam(paramType = "path", name = "uuid", dataType = "String", required = true, value = "用户UUID", defaultValue = ""),
+			@ApiImplicitParam(paramType = "query", name = "orderId", dataType = "String", required = true, value = "订单编号", defaultValue = "1231230001000001513657092497")
+	})
+	@ApiResponses({ 
+			@ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 204, message = "OK"),
+			@ApiResponse(code = 400, message = "请求参数没填好"), @ApiResponse(code = 401, message = "未授权用户"),
+			@ApiResponse(code = 403, message = "服务器已经理解请求，但是拒绝执行它"),
+			@ApiResponse(code = 404, message = "请求路径没有或页面跳转路径不对") })
+	@RequestMapping(value = "/funds/buyDetails/{orderId}", method = RequestMethod.GET)
+	public ResponseEntity<Map> buyDetails(
+			// @PathVariable(value = "groupId") Long uuid,
+			@RequestParam(value = "orderId") String orderId) throws Exception {
+		logger.error("method buyDetails run ..");
+		Map<String, Object> result = new HashMap<String, Object>();
+		if(StringUtils.isEmpty(orderId)){
+			logger.error("购买详情不存在:"+orderId);
+			throw new Exception("购买详情不存在:"+orderId);
+		}
+		TrdOrder trdOrder = orderService.getOrderByOrderId(orderId);
+		List<TrdOrderDetail> trdOrderDetailList = new ArrayList<TrdOrderDetail>();
+		if (trdOrder != null && trdOrder.getOrderId() != null) {
+			trdOrderDetailList = orderService.findOrderDetailByOrderId(orderId);
+		} else {
+			logger.error("购买详情不存在.");
+			throw new Exception("购买详情不存在.");
+		}
+		//result.put("list", trdOrderDetailList);
+		if(trdOrderDetailList==null||trdOrderDetailList.isEmpty()){
+			logger.error("购买详情信息数据不存在.");
+			throw new Exception("购买详情信息数据不存在.");
+		}
+		Map<String,Object> trdOrderMap = new HashMap<String,Object>();
+		if(trdOrder.getOrderStatus() == 0){
+			result.put("orderStatus", "待确认");
+		} else if(trdOrder.getOrderStatus() == 1){
+			result.put("orderStatus", "已确认");
+		} else {
+			logger.info("状态为："+trdOrder.getOrderStatus());
+			result.put("orderStatus", "交易失败");
+		}
+		//TODO title
+		result.put("title", "稳健型-3个月组合");
+		//金额
+		result.put("amount", trdOrder.getPayAmount());
+		//手续费
+		result.put("payfee", trdOrder.getPayFee());
+		Calendar c = Calendar.getInstance();
+		List<Map<String,Object>> statusList = new ArrayList<Map<String,Object>>();
+		Map<String,Object> statusMap = new HashMap<String,Object>();
+		statusMap.put("time", c.get(Calendar.HOUR_OF_DAY)+":"+c.get(Calendar.MINUTE));
+		statusMap.put("date", c.get(Calendar.YEAR)+"."+c.get(Calendar.MONTH)+"."+c.get(Calendar.DATE));
+		statusMap.put("status", "申请已受理");
+		statusList.add(statusMap);
+		
+		statusMap = new HashMap<String,Object>();
+		statusMap.put("time", c.get(Calendar.HOUR_OF_DAY)+":"+c.get(Calendar.MINUTE));
+		statusMap.put("date", c.get(Calendar.YEAR)+"."+c.get(Calendar.MONTH)+"."+(c.get(Calendar.DATE)+2));
+		statusMap.put("status", "份额确认，开始计算收益");
+		statusList.add(statusMap);
+		
+		statusMap = new HashMap<String,Object>();
+		statusMap.put("time", c.get(Calendar.HOUR_OF_DAY)+":"+c.get(Calendar.MINUTE));
+		statusMap.put("date", c.get(Calendar.YEAR)+"."+c.get(Calendar.MONTH)+"."+(c.get(Calendar.DATE)+3));
+		statusMap.put("status", "查看收益");
+		statusList.add(statusMap);
+		
+		//状态详情
+		List<Map<String,Object>> detailList = new ArrayList<Map<String,Object>>();
+		Map<String,Object> detailMap = new HashMap<String,Object>();
+		for(int i=0;i<trdOrderDetailList.size();i++){
+			detailMap = new HashMap<String,Object>();
+			TrdOrderDetail trdOrderDetail = trdOrderDetailList.get(0);
+			
+		}
+		
+		return new ResponseEntity<Map>(result, HttpStatus.OK);
+	}
 }
--- aaas-finance-trade/aaas-finance-trade-order/src/main/java/com/shellshellfish/aaas/finance/trade/order/service/OrderService.java
+++ aaas-finance-trade/aaas-finance-trade-order/src/main/java/com/shellshellfish/aaas/finance/trade/order/service/OrderService.java
@@ -1,15 +1,12 @@
 package com.shellshellfish.aaas.finance.trade.order.service;
 
+import com.shellshellfish.aaas.finance.trade.order.model.dao.TrdOrder;
 import com.shellshellfish.aaas.finance.trade.order.model.dao.TrdOrderDetail;
 import java.util.List;
 
 public interface OrderService {
-
-  List<TrdOrderDetail> getOrderByUserId(Long userId);
-
-
-
-
-
+	List<TrdOrderDetail> getOrderByUserId(Long userId);
+	List<TrdOrderDetail> findOrderDetailByOrderId(String orderId);
+	TrdOrder getOrderByOrderId(String orderId);
 
 }
