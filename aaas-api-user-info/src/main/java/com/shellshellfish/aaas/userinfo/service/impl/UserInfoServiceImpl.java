25c71c96e2d85100c15b20eb44b953bdb650e43c
shitong <tong.shi@shellshellfish.com>
2018-02-01 17:58:07 +0800

    201802011758

--- aaas-api-user-info/src/main/java/com/shellshellfish/aaas/userinfo/service/impl/UserInfoServiceImpl.java
+++ aaas-api-user-info/src/main/java/com/shellshellfish/aaas/userinfo/service/impl/UserInfoServiceImpl.java
@@ -697,6 +697,8 @@ public class UserInfoServiceImpl implements UserInfoService {
 					.getProductDetailsByProdId(products.getId());
 			Integer count = 0;
 			Integer fails = 0;
+			Integer statusIsNull = 0;
+			
 			if (productDetailsList != null && productDetailsList.size() > 0) {
 				for (int j = 0; j < productDetailsList.size(); j++) {
 					UiProductDetailDTO uiProductDetailDTO = productDetailsList.get(j);
@@ -708,11 +710,19 @@ public class UserInfoServiceImpl implements UserInfoService {
 						} else if (uiProductDetailDTO.getStatus() == TrdOrderStatusEnum.FAILED.getStatus()) {
 							fails++;
 						}
+					} else {
+						statusIsNull++;
 					}
 				}
 				if (fails > 0) {
 					if (fails == productDetailsList.size()) {
 						//若组合中全部失败，则不显示
+						continue;
+					}
+				}
+				if (statusIsNull > 0) {
+					if (statusIsNull == productDetailsList.size()) {
+						//若组合中状态全部为NULL，则不显示
 						continue;
 					}
 				}
