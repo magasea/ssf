04c952954071f11f8e0c223efa530ea9aa3d593c
chenwei <magaseaocp@gmail.com>
2018-03-28 15:31:48 +0800

    fix bug

--- aaas-api-asset-allocation/src/main/java/com/shellshellfish/aaas/assetallocation/configuration/DataSourceConfig.java
+++ aaas-api-asset-allocation/src/main/java/com/shellshellfish/aaas/assetallocation/configuration/DataSourceConfig.java
@@ -1,12 +1,31 @@
 package com.shellshellfish.aaas.assetallocation.configuration;
 
-import com.alibaba.druid.pool.DruidDataSource;
+
+import com.zaxxer.hikari.HikariDataSource;
+import java.util.Properties;
+import javax.naming.NamingException;
+import javax.persistence.EntityManagerFactory;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * Author: hongming
@@ -16,42 +35,96 @@ import javax.sql.DataSource;
 @ComponentScan
 @Configuration
 @ConfigurationProperties(prefix="spring.datasource")
+
+
+
+@EnableTransactionManagement
+
 public class DataSourceConfig {
 
-    private String url;
-    private String username;
-    private String password;
+    @Autowired
+    private Environment environment;
 
+    @Value("${datasource.sampleapp.maxPoolSize:10}")
+    private int maxPoolSize;
+
+    /*
+     * Populate SpringBoot DataSourceProperties object directly from application.yml
+     * based on prefix.Thanks to .yml, Hierachical data is mapped out of the box with matching-name
+     * properties of DataSourceProperties object].
+     */
     @Bean
-    public DataSource getDataSource() {
-        DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
+    @Primary
+    @ConfigurationProperties(prefix = "datasource.sampleapp")
+    public DataSourceProperties dataSourceProperties(){
+        return new DataSourceProperties();
+    }
+
+    /*
+     * Configure HikariCP pooled DataSource.
+     */
+    @Bean
+    public DataSource dataSource() {
+        DataSourceProperties dataSourceProperties = dataSourceProperties();
+        HikariDataSource dataSource = (HikariDataSource) DataSourceBuilder
+            .create(dataSourceProperties.getClassLoader())
+            .driverClassName(dataSourceProperties.getDriverClassName())
+            .url(dataSourceProperties.getUrl())
+            .username(dataSourceProperties.getUsername())
+            .password(dataSourceProperties.getPassword())
+            .type(HikariDataSource.class)
+            .build();
+        dataSource.setMaximumPoolSize(maxPoolSize);
         return dataSource;
     }
 
-    public String getUrl() {
-        return url;
+    /*
+     * Entity Manager Factory setup.
+     */
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws NamingException {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setPackagesToScan(new String[] { "com.shellshellfish.aaas.assetallocation" });
+        factoryBean.setJpaVendorAdapter(jpaVendorAdapter());
+//        factoryBean.setJpaProperties(jpaProperties());
+
+
+
+        return factoryBean;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
+    /*
+     * Provider specific adapter.
+     */
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        return hibernateJpaVendorAdapter;
     }
 
-    public String getUsername() {
-        return username;
+    /*
+     * Here you can specify any provider specific properties.
+     */
+//    private java.util.Properties jpaProperties() {
+//        java.util.Properties properties = new Properties();
+//        properties.put("hibernate.dialect", environment.getRequiredProperty("datasource.sampleapp.hibernate.dialect"));
+//        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("datasource.sampleapp.hibernate.hbm2ddl.method"));
+//        properties.put("hibernate.show_sql", environment.getRequiredProperty("datasource.sampleapp.hibernate.show_sql"));
+//        properties.put("hibernate.format_sql", environment.getRequiredProperty("datasource.sampleapp.hibernate.format_sql"));
+//        if(StringUtils.isNotEmpty(environment.getRequiredProperty("datasource.sampleapp.defaultSchema"))){
+//            properties.put("hibernate.default_schema", environment.getRequiredProperty("datasource.sampleapp.defaultSchema"));
+//        }
+//        return properties;
+//    }
+
+    @Bean
+    @Autowired
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(emf);
+        return txManager;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
+
