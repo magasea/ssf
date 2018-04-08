fe0136a77d1b7b9b1c27e88e0ecbd671b27eb0dc
chenyupan <pierre.chen@shellshellfish.com>
2018-04-08 18:19:30 +0800

    bugFix

--- aaas-api-user-info/src/main/java/com/shellshellfish/aaas/userinfo/service/impl/CalculateConfirmedAsset.java
+++ aaas-api-user-info/src/main/java/com/shellshellfish/aaas/userinfo/service/impl/CalculateConfirmedAsset.java
@@ -33,78 +33,80 @@ import org.springframework.stereotype.Service;
 @Service
 public class CalculateConfirmedAsset {
 
-	Logger logger = LoggerFactory.getLogger(CalculateConfirmedAsset.class);
+  Logger logger = LoggerFactory.getLogger(CalculateConfirmedAsset.class);
 
-	@Autowired
-	UserFinanceProdCalcService userFinanceProdCalcService;
+  @Autowired
+  UserFinanceProdCalcService userFinanceProdCalcService;
 
-	@Autowired
-	UiProductDetailRepo uiProductDetailRepo;
+  @Autowired
+  UiProductDetailRepo uiProductDetailRepo;
 
-	@Autowired
-	UiProductRepo uiProductRepo;
+  @Autowired
+  UiProductRepo uiProductRepo;
 
-	@Autowired
-	@Qualifier("zhongZhengMongoTemplate")
-	MongoTemplate zhongZhengMongoTemplate;
+  @Autowired
+  @Qualifier("zhongZhengMongoTemplate")
+  MongoTemplate zhongZhengMongoTemplate;
 
-	@Autowired
-	UserInfoRepository userInfoRepository;
+  @Autowired
+  UserInfoRepository userInfoRepository;
 
-	/**
-	 * 当有申购或者赎回确认的消息时 ,重新计算资产
-	 */
-	public void calculateConfirmedAsset(MongoUiTrdZZInfo mongoUiTrdZZInfo) {
-		UiProducts uiProducts = uiProductRepo.findById(mongoUiTrdZZInfo.getUserProdId());
-		List<UiProductDetail> uiProductDetailList = uiProductDetailRepo
-				.findAllByUserProdId(mongoUiTrdZZInfo.getUserProdId());
-		String date = mongoUiTrdZZInfo.getConfirmDate();
+  /**
+   * 当有申购或者赎回确认的消息时 ,重新计算资产
+   */
+  public void calculateConfirmedAsset(MongoUiTrdZZInfo mongoUiTrdZZInfo) {
+    final String pattern = "yyyyMMdd";
+    UiProducts uiProducts = uiProductRepo.findById(mongoUiTrdZZInfo.getUserProdId());
+    List<UiProductDetail> uiProductDetailList = uiProductDetailRepo
+        .findAllByUserProdId(mongoUiTrdZZInfo.getUserProdId());
+    String date = mongoUiTrdZZInfo.getConfirmDate();
 
-		LocalDate  now = LocalDate.now(ZoneId.systemDefault()).plusDays(1);
-		LocalDate confirmDate = InstantDateUtil.format(date,"yyyyMMdd");
-		for (;confirmDate.isBefore(now);confirmDate=confirmDate.plusDays(1)){
-			//从确认日期开始到当前时间的数据都要修正（此处默认此次确认到当前时间没有其他操作）
-			String uuid = Optional.ofNullable(userInfoRepository.findById(mongoUiTrdZZInfo.getUserId()))
-					.map(m -> m.getUuid()).orElse("-1");
-			for (UiProductDetail uiProductDetail : uiProductDetailList) {
-				try {
-					userFinanceProdCalcService
-							.calculateFromZzInfo(uiProductDetail, uuid, uiProducts.getProdId(), date);
-				} catch (Exception e) {
-					logger.error("calculate dailyAmount failed:{}", uiProductDetail, e);
-				}
-			}
-			updateDailyAmountFromZzInfo(uuid, uiProducts.getProdId(), uiProducts.getId(),
-					mongoUiTrdZZInfo.getFundCode(), date,
-					TradeUtil.getBigDecimalNumWithDiv100(mongoUiTrdZZInfo.getTradeConfirmSum()),
-					mongoUiTrdZZInfo.getTradeType());
-		}
-	}
+    LocalDate now = LocalDate.now(ZoneId.systemDefault()).plusDays(1);
+    LocalDate confirmDate = InstantDateUtil.format(date, pattern);
+    for (; confirmDate.isBefore(now); confirmDate = confirmDate.plusDays(1)) {
+      //从确认日期开始到当前时间的数据都要修正（此处默认此次确认到当前时间没有其他操作）
+      String uuid = Optional.ofNullable(userInfoRepository.findById(mongoUiTrdZZInfo.getUserId()))
+          .map(m -> m.getUuid()).orElse("-1");
+      for (UiProductDetail uiProductDetail : uiProductDetailList) {
+        try {
+          userFinanceProdCalcService
+              .calculateFromZzInfo(uiProductDetail, uuid, uiProducts.getProdId(),
+                  InstantDateUtil.format(confirmDate, pattern));
+        } catch (Exception e) {
+          logger.error("calculate dailyAmount failed:{}", uiProductDetail, e);
+        }
+      }
+      updateDailyAmountFromZzInfo(uuid, uiProducts.getProdId(), uiProducts.getId(),
+          mongoUiTrdZZInfo.getFundCode(), InstantDateUtil.format(confirmDate, pattern),
+          TradeUtil.getBigDecimalNumWithDiv100(mongoUiTrdZZInfo.getTradeConfirmSum()),
+          mongoUiTrdZZInfo.getTradeType());
+    }
+  }
 
-	private void updateDailyAmountFromZzInfo(String userUuid, Long prodId, Long userProdId,
-			String fundCode, String startDate, BigDecimal amount, int type) {
+  private void updateDailyAmountFromZzInfo(String userUuid, Long prodId, Long userProdId,
+      String fundCode, String startDate, BigDecimal amount, int type) {
 
-		Query query = new Query();
-		query.addCriteria(Criteria.where("userUuid").is(userUuid))
-				.addCriteria(Criteria.where("date").is(startDate))
-				.addCriteria(Criteria.where("fundCode").is(fundCode))
-				.addCriteria(Criteria.where("prodId").is(prodId))
-				.addCriteria(Criteria.where("userProdId").is(userProdId));
+    Query query = new Query();
+    query.addCriteria(Criteria.where("userUuid").is(userUuid))
+        .addCriteria(Criteria.where("date").is(startDate))
+        .addCriteria(Criteria.where("fundCode").is(fundCode))
+        .addCriteria(Criteria.where("prodId").is(prodId))
+        .addCriteria(Criteria.where("userProdId").is(userProdId));
 
-		Update update = new Update();
+    Update update = new Update();
 
-		if (TrdOrderOpTypeEnum.BUY.getOperation() == type) {
-			update.set("buyAmount", amount);
-		}
-		if (TrdOrderOpTypeEnum.REDEEM.getOperation() == type) {
-			update.set("sellAmount", amount);
-		}
+    if (TrdOrderOpTypeEnum.BUY.getOperation() == type) {
+      update.set("buyAmount", amount);
+    }
+    if (TrdOrderOpTypeEnum.REDEEM.getOperation() == type) {
+      update.set("sellAmount", amount);
+    }
 
-		DailyAmount dailyAmount = zhongZhengMongoTemplate
-				.findAndModify(query, update, new FindAndModifyOptions().returnNew(true),
-						DailyAmount.class);
-		logger.info(
-				"set buyAmount and sell Amount ==> dailyAmount:{}", dailyAmount);
+    DailyAmount dailyAmount = zhongZhengMongoTemplate
+        .findAndModify(query, update, new FindAndModifyOptions().returnNew(true),
+            DailyAmount.class);
+    logger.info(
+        "set buyAmount and sell Amount ==> dailyAmount:{}", dailyAmount);
 
-	}
+  }
 }
