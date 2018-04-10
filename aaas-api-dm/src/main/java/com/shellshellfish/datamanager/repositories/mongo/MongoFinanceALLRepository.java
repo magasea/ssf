6c5427c0acf4e1ac2c59302251278049570fa001
shitong <tong.shi@shellshellfish.com>
2018-04-10 16:04:59 +0800

    st_1000263_20180410

--- aaas-api-dm/src/main/java/com/shellshellfish/datamanager/repositories/mongo/MongoFinanceALLRepository.java
+++ aaas-api-dm/src/main/java/com/shellshellfish/datamanager/repositories/mongo/MongoFinanceALLRepository.java
@@ -1,9 +1,11 @@
 package com.shellshellfish.datamanager.repositories.mongo;
 
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.shellshellfish.datamanager.model.MongoFinanceAll;
 
 public interface MongoFinanceALLRepository extends MongoRepository<MongoFinanceAll, Long> {
 	MongoFinanceAll findAllByDate(String date);
 	void deleteAllByDate(String date);
+	List<MongoFinanceAll> findBySerialIn(List<Integer> serial);
 }
