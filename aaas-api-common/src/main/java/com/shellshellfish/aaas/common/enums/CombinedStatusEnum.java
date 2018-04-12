8741cde6fc105f59218fff366982dfa4693ec791
shitong <tong.shi@shellshellfish.com>
2018-04-12 17:18:54 +0800

    fix bug

--- aaas-api-common/src/main/java/com/shellshellfish/aaas/common/enums/CombinedStatusEnum.java
+++ aaas-api-common/src/main/java/com/shellshellfish/aaas/common/enums/CombinedStatusEnum.java
@@ -1,7 +1,7 @@
 package com.shellshellfish.aaas.common.enums;
 
 public enum CombinedStatusEnum {
-	WAITCONFIRM(0, "确认中"),CONFIRMED(2, "确认成功"),CONFIRMEDFAILED(-1, "确认失败");
+	WAITCONFIRM(0, "确认中"),CONFIRMED(2, "确认成功"),CONFIRMEDFAILED(-1, "确认失败"),SOMECONFIRMED(-1, "部分确认成功");
 
 	private int operation;
 	String comment;
