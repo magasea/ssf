e7555566c77edc90c942f4614d921a99feacab84
chenwei <chenwei@shellshellfish.com>
2018-04-20 15:39:35 +0800

    update for finance run time error

---
+++ aaas-finance-trade/aaas-finance-trade-order/src/main/java/com/shellshellfish/aaas/finance/trade/order/service/BankDicService.java
@@ -0,0 +1,10 @@
+package com.shellshellfish.aaas.finance.trade.order.service;
+
+/**
+ * Created by chenwei on 2018- 四月 - 20
+ */
+public interface BankDicService {
+
+  public void checkZhongZhenSupportedBanks();
+
+}
