28b7fd2bbce5e9d96f9e1cede3a8014502705780
shitong <tong.shi@shellshellfish.com>
2018-04-24 11:56:56 +0800

    fix bug

--- aaas-api-dm/src/main/java/com/shellshellfish/aaas/datamanager/service/OptimizationService.java
+++ aaas-api-dm/src/main/java/com/shellshellfish/aaas/datamanager/service/OptimizationService.java
@@ -5,7 +5,7 @@ import com.shellshellfish.aaas.datamanager.model.JsonResult;
 public interface OptimizationService {
 	JsonResult financeFront(Integer oemid);
 	JsonResult getFinanceFront();
-	JsonResult checkPrdDetails(String groupId, String subGroupId);
+	JsonResult checkPrdDetails(String groupId, String subGroupId, Integer oemid);
 	JsonResult getPrdDetails(String groupId, String subGroupId, Integer oemid);
     JsonResult checkPrdDetails2(String groupId, String subGroupId, Integer oemid);
     JsonResult getFinanceFront(Integer size, Integer pageSize, Integer oemid);
