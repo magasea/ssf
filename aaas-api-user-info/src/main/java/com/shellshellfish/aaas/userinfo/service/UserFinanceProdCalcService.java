0e479d9a8e119121962f9b724e0bb8eaff21f2a0
chenyupan <pierre.chen@shellshellfish.com>
2018-05-18 16:28:53 +0800

    优化dailyAsset 计算

--- aaas-api-user-info/src/main/java/com/shellshellfish/aaas/userinfo/service/UserFinanceProdCalcService.java
+++ aaas-api-user-info/src/main/java/com/shellshellfish/aaas/userinfo/service/UserFinanceProdCalcService.java
@@ -3,47 +3,42 @@ package com.shellshellfish.aaas.userinfo.service;
 import com.shellshellfish.aaas.userinfo.model.PortfolioInfo;
 import com.shellshellfish.aaas.userinfo.model.dao.UiProductDetail;
 import com.shellshellfish.aaas.userinfo.model.dao.UiUser;
+
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
 public interface UserFinanceProdCalcService {
 
+    void calcIntervalAmount(String userUuid, Long prodId, String fundCode, String startDate)
+            throws Exception;
 
-	BigDecimal calcTotalDailyAsset(String userUuid) throws Exception;
+    void initDailyAmount(String userUuid, Long prodId, Long userProdId, String date, String fundCode);
 
-	BigDecimal calcDailyAsset(String userUuid, Long prodId, String fundCode, String date)
-			throws Exception;
+    BigDecimal calcYieldValue(String userUuid, Long prodId, String startDate, String endDate);
 
-	void calcIntervalAmount(String userUuid, Long prodId, String fundCode, String startDate)
-			throws Exception;
+    BigDecimal calcYieldRate(String userUuid, Long prodId, String startDate, String endDate);
 
-	void initDailyAmount(String userUuid, Long prodId, Long userProdId, String date, String fundCode);
+    BigDecimal calcYieldValue(String userUuid, String startDate, String endDate);
 
-	BigDecimal calcYieldValue(String userUuid, Long prodId, String startDate, String endDate);
+    BigDecimal calcYieldRate(String userUuid, String startDate, String endDate);
 
-	BigDecimal calcYieldRate(String userUuid, Long prodId, String startDate, String endDate);
+    void dailyCalculation() throws Exception;
 
-	BigDecimal calcYieldValue(String userUuid, String startDate, String endDate);
+    void dailyCalculation(String date) throws Exception;
 
-	BigDecimal calcYieldRate(String userUuid, String startDate, String endDate);
+    void dailyCalculation(String date, List<UiUser> uiUsers) throws Exception;
 
-	void dailyCalculation() throws Exception;
+    BigDecimal getAssert(String userUuid, Long prodId) throws Exception;
 
-	void dailyCalculation(String date) throws Exception;
+    PortfolioInfo calculateProductValue(String userUuid, Long prodId,
+                                        String startDate, String endDate);
 
-	void dailyCalculation(String date, List<UiUser> uiUsers) throws Exception;
+    List<Map<String, Object>> getCalcYieldof7days(String fundCode, String type, String date)
+            throws Exception;
 
-	BigDecimal getAssert(String userUuid, Long prodId) throws Exception;
+    void calculateProductAsset(UiProductDetail detail, String uuid, Long prodId, String date);
 
-	PortfolioInfo calculateProductValue(String userUuid, Long prodId,
-			String startDate, String endDate);
-
-	List<Map<String, Object>> getCalcYieldof7days(String fundCode, String type, String date)
-			throws Exception;
-
-	void calculateProductAsset(UiProductDetail detail, String uuid, Long prodId, String date);
-
-	void calculateFromZzInfo(UiProductDetail detail, String uuid, Long prodId, String date)
-			throws Exception;
+    void calculateFromZzInfo(UiProductDetail detail, String uuid, Long prodId, String date)
+            throws Exception;
 }
