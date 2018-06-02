d55813fc995d4b371ea4dca7f844d798cb79447e
chenyupan <pierre.chen@shellshellfish.com>
2018-06-02 16:37:16 +0800

    add tool to fill up mongo_fund_base_close

--- aaas-api-dm/src/main/java/com/shellshellfish/aaas/datamanager/controller/DemoController.java
+++ aaas-api-dm/src/main/java/com/shellshellfish/aaas/datamanager/controller/DemoController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Api("checkFundBaseClose")
 @RestController
+@RequestMapping("/api/datamanager/")
 public class DemoController {
 
     @Autowired
