c7965d5a6c9a8fb85d81c2e5b7fb0bcb956d239e
chenyupan <pierre.chen@shellshellfish.com>
2018-06-02 16:26:30 +0800

    add tool to fill up mongo_fund_base_close

--- aaas-api-dm/src/main/java/com/shellshellfish/aaas/datamanager/repositories/mongo/MongoFundBaseCloseRepository.java
+++ aaas-api-dm/src/main/java/com/shellshellfish/aaas/datamanager/repositories/mongo/MongoFundBaseCloseRepository.java
@@ -2,7 +2,9 @@ package com.shellshellfish.aaas.datamanager.repositories.mongo;
 
 
 import com.shellshellfish.aaas.datamanager.model.FundBaseClose;
+
 import java.util.List;
+
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +12,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MongoFundBaseCloseRepository extends MongoRepository<FundBaseClose, Long> {
 
-	List<FundBaseClose> findByQueryDateBetween(Long startDate, Long endDate, Sort sort);
+    List<FundBaseClose> findByQueryDateBetween(Long startDate, Long endDate, Sort sort);
 }
