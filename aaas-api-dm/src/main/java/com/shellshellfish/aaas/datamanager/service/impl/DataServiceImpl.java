f7f808892fb0778ae573ba9f8b6e9d5ba343715a
chenyupan <pierre.chen@shellshellfish.com>
2018-06-07 13:37:49 +0800

    货币基金你复权单位净值如果为空，则默认为1

--- aaas-api-dm/src/main/java/com/shellshellfish/aaas/datamanager/service/impl/DataServiceImpl.java
+++ aaas-api-dm/src/main/java/com/shellshellfish/aaas/datamanager/service/impl/DataServiceImpl.java
@@ -1018,7 +1018,7 @@ public class DataServiceImpl implements DataService {
             map.put("yieldOf7Days", coinFundYieldRate.getYieldOf7Days());
             map.put("tenKiloUnitYield", coinFundYieldRate.getTenKiloUnityYield());
 
-            BigDecimal todayNavAdj = Optional.ofNullable(coinFundYieldRate).map(m -> m.getNavAdj()).orElse(BigDecimal.ZERO);
+            BigDecimal todayNavAdj = Optional.ofNullable(coinFundYieldRate).map(m -> m.getNavAdj()).orElse(BigDecimal.ONE);
             map.put("navAdj", todayNavAdj);
 
             BigDecimal dayUp;
