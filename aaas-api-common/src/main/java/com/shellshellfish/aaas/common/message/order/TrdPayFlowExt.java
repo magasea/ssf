5ab6795cb3920b6909493126b573b7a00c88860a
chenwei <chenwei@shellshellfish.com>
2018-06-28 20:59:11 +0800

    fix serial issue

--- aaas-api-common/src/main/java/com/shellshellfish/aaas/common/message/order/TrdPayFlowExt.java
+++ aaas-api-common/src/main/java/com/shellshellfish/aaas/common/message/order/TrdPayFlowExt.java
@@ -1,10 +1,12 @@
 package com.shellshellfish.aaas.common.message.order;
 
+import java.io.Serializable;
+
 /**
  * Created by developer4 on 2018- 六月 - 28
  */
 
-public class TrdPayFlowExt {
+public class TrdPayFlowExt implements Serializable {
   String confirmDateExpected ;
 
   public String getConfirmDateExpected() {
