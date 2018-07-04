cbed79581e8c3b7170e33e34d14c7aa715aa0af0
chenwei <chenwei@shellshellfish.com>
2018-07-04 17:02:11 +0800

    fix exception condition

--- aaas-api-user-info/src/main/java/com/shellshellfish/aaas/userinfo/service/impl/CheckPendingRecordsServiceImpl.java
+++ aaas-api-user-info/src/main/java/com/shellshellfish/aaas/userinfo/service/impl/CheckPendingRecordsServiceImpl.java
@@ -88,7 +88,8 @@ public class CheckPendingRecordsServiceImpl implements CheckPendingRecordsServic
       }else if(mongoPendingRecord.getUserProdId() == null && mongoPendingRecord.getUserProdId() <=
           0){
         mongoTemplate.remove(mongoPendingRecord);
-      } else if(StringUtils.isEmpty(mongoPendingRecord.getOutsideOrderId())) {
+      } else if(StringUtils.isEmpty(mongoPendingRecord.getOutsideOrderId())||
+          StringUtils.isEmpty(mongoPendingRecord.getFundCode()) ) {
         logger.error("empty order id is not valid record");
         mongoTemplate.remove(mongoPendingRecord);
       }else{
@@ -102,9 +103,12 @@ public class CheckPendingRecordsServiceImpl implements CheckPendingRecordsServic
   @Transactional
   @Override
   public void processPendingRecord(MongoPendingRecords mongoPendingRecords){
-    List<TrdOrderDetail> trdOrderDetails = orderRpcService
-        .getOrderDetailByUserProdIdAndFundCodeAndTrdType(mongoPendingRecords
-        .getUserProdId(), mongoPendingRecords.getFundCode(), mongoPendingRecords.getTradeType());
+    try {
+      List<TrdOrderDetail> trdOrderDetails = orderRpcService
+          .getOrderDetailByUserProdIdAndFundCodeAndTrdType(mongoPendingRecords
+              .getUserProdId(), mongoPendingRecords.getFundCode(), mongoPendingRecords.getTradeType());
+
+
 //    mongoPendingRecords.getUserProdId();
     //because if there is a pendingRecord with the UserProdId and the FundCode without orderId, then
     //further operation on the same userProdId or the same prodId will be blocked
@@ -150,6 +154,9 @@ public class CheckPendingRecordsServiceImpl implements CheckPendingRecordsServic
       }else{
         mongoTemplate.remove(mongoPendingRecords);
       }
+    }
+    }catch (Exception ex){
+      logger.error("failed to process pendingRecord:", ex);
     }
   }
 
