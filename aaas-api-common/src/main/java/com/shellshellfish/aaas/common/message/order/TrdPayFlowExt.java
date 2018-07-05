4d80d395e0f016abfba47ffa1e094cc76cd75940
chenwei <chenwei@shellshellfish.com>
2018-07-05 16:22:09 +0800

    adjust date format

--- aaas-api-common/src/main/java/com/shellshellfish/aaas/common/message/order/TrdPayFlowExt.java
+++ aaas-api-common/src/main/java/com/shellshellfish/aaas/common/message/order/TrdPayFlowExt.java
@@ -1,6 +1,7 @@
 package com.shellshellfish.aaas.common.message.order;
 
 import java.io.Serializable;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * Created by developer4 on 2018- 六月 - 28
@@ -10,6 +11,12 @@ public class TrdPayFlowExt implements Serializable {
   String confirmDateExpected ;
 
   public String getConfirmDateExpected() {
+    if(!StringUtils.isEmpty(confirmDateExpected) && !confirmDateExpected.contains("-")){
+      if(confirmDateExpected.length() >= 8){
+        return confirmDateExpected.substring(0,4)+"-"+confirmDateExpected.substring(4,6)+"-" +
+            confirmDateExpected.substring(6,8);
+      }
+    }
     return confirmDateExpected;
   }
 
